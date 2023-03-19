a78660b4731d538d0a955ca23a6442009064a53c
Boris Berman <bermanboris08@gmail.com>
2023-03-19 17:02:03 +0100

    format backprop file

--- backprop.py
+++ backprop.py
@@ -2,12 +2,13 @@ import numpy as np
 from autograd.misc import flatten
 
 # Backpropagation step.
+
 ##
 # ADAM Optimizer
 ##
+
 def exponential_decay(step_size):
-  if step_size > 0.001:
-      step_size *= 0.999
+  if step_size > 0.001: step_size *= 0.999
   return step_size
 
 def adam_minmin(grad_both, init_params_nn, init_params_nn2, callback=None, num_iters=100, step_size=0.001, b1=0.9, b2=0.999, eps=10**-8):
@@ -16,6 +17,7 @@ def adam_minmin(grad_both, init_params_nn, init_params_nn2, callback=None, num_i
 
   m_nn, v_nn = np.zeros(len(x_nn)), np.zeros(len(x_nn))
   m_nn2, v_nn2 = np.zeros(len(x_nn2)), np.zeros(len(x_nn2))
+
   for i in range(num_iters):
     g_nn_uf, g_nn2_uf = grad_both(unflatten_nn(x_nn), unflatten_nn2(x_nn2), i)
     g_nn, _ = flatten(g_nn_uf)
@@ -39,4 +41,5 @@ def adam_minmin(grad_both, init_params_nn, init_params_nn2, callback=None, num_i
     mhat_nn2 = m_nn2 / (1 - b1**(i + 1))    # Bias correction.
     vhat_nn2 = v_nn2 / (1 - b2**(i + 1))
     x_nn2 = x_nn2 - step_size * mhat_nn2 / (np.sqrt(vhat_nn2) + eps)
+
   return unflatten_nn(x_nn), unflatten_nn2(x_nn2)
