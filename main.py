bd881de7c9308196a2cc9e4774a43bc992b6f057
Boris Berman <bermanboris08@gmail.com>
2023-03-23 14:48:36 +0100

    add indelphi and main files

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import numpy
+import inDelphi
+
+left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
+right_seq = 'CCGTGGACGTGAGAAAGAAGAAACATAATATTCGCACTAGATCCATCCCCATACCTGACC'
+seq = left_seq + right_seq
+cutsite = len(left_seq)
+
+pred_df, stats = inDelphi.predict(seq, cutsite)
+
+print(seq)
