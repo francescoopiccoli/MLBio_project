ce3c53285dc1558e2ba5f5ebeb8e7c966681242b
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-03-23 18:54:27 +0100

    chore: make print nicer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy
 import inDelphi
+import pandas as pd
 
 left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
 right_seq = 'CCGTGGACGTGAGAAAGAAGAAACATAATATTCGCACTAGATCCATCCCCATACCTGACC'
@@ -9,5 +10,9 @@ seq = left_seq + right_seq
 cutsite = len(left_seq)
 
 pred_df, stats = inDelphi.predict(seq, cutsite)
-print(pred_df)
-print(stats)
+
+pd.options.display.float_format = '{:.2f}'.format
+pred_df = inDelphi.add_genotype_column(pred_df, stats)
+print(pred_df.tail(4))
+pred_df.sort_values(by = 'Predicted frequency', ascending = False, inplace = True)
+print(pred_df.head(10))
