c4af15da96f55267791e4fec4cb2fc06d9d18a40
Wessel Oosterbroek <w.oosterbroek@student.tudelft.nl>
2023-03-26 17:19:21 +0200

    Fixed genotype not being printed correctly, cleanup

--- generate_figures.py
+++ generate_figures.py
@@ -1,9 +1,6 @@
 from inDelphiModel import inDelphi
 import pandas as pd
 
-inDelphi.init_model(celltype = 'mESC')
-
-
 # Generate figure 1e
 def generate_figure_1e(sequence, cutsite):
 
@@ -11,21 +8,22 @@ def generate_figure_1e(sequence, cutsite):
     # Get predictions and statistics
     pred_df, stats = inDelphi.predict(sequence, cutsite)
 
-    # Adds a genotype colum
-    pred_df = inDelphi.add_genotype_column(pred_df, stats)
-
     # Adds the mhless_genotypes
     pred_df = inDelphi.add_mhless_genotypes(pred_df, stats)
+
+    # Adds a genotype colum
+    pred_df = inDelphi.add_genotype_column(pred_df, stats)
     
-    print(pred_df.sort_values(by='Predicted frequency', ascending=False).head(20))
-    print(stats)
+    # Print data frame
+    print(pred_df.sort_values(by='Predicted frequency', ascending=False).head(20)[['Genotype', 'Predicted frequency']].to_string(index=False))
+    # print(stats)
+
     pd.reset_option('display.max_rows')
 
 
 
 def generate_figure_3f(predictions):
-
-    pass
+  pass
   # Get highest ins rate for each sequence
 
   # Get highest deletion rate for each sequence
@@ -33,9 +31,14 @@ def generate_figure_3f(predictions):
   # Plot them
 
 if __name__ == '__main__':
+
+    # Init inDelphi with mouse cell dataset
+    inDelphi.init_model(celltype = 'mESC')
+
     left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
     right_seq = 'CCGTGGACGTGAGAAAGAAGAAACATAATATTCGCACTAGATCCATCCCCATACCTGACC'
     seq = left_seq + right_seq
     cutsite = len(left_seq)
 
+    # Generate figure 1e
     generate_figure_1e(seq, cutsite)
