8fe77a0292986633c557fefdb317e2ef02e98ab5
Wessel Oosterbroek <w.oosterbroek@student.tudelft.nl>
2023-03-26 17:13:48 +0200

    Print full sequence, add MH-less sequences

--- generate_figures.py
+++ generate_figures.py
@@ -7,17 +7,19 @@ inDelphi.init_model(celltype = 'mESC')
 # Generate figure 1e
 def generate_figure_1e(sequence, cutsite):
 
-    
 
-
+    # Get predictions and statistics
     pred_df, stats = inDelphi.predict(sequence, cutsite)
 
     # Adds a genotype colum
     pred_df = inDelphi.add_genotype_column(pred_df, stats)
 
-    print(pred_df['Genotype'])
+    # Adds the mhless_genotypes
+    pred_df = inDelphi.add_mhless_genotypes(pred_df, stats)
+    
+    print(pred_df.sort_values(by='Predicted frequency', ascending=False).head(20))
     print(stats)
-
+    pd.reset_option('display.max_rows')
 
 
 
