c2e11b601ba02fa1648ff040590114cd4d03530d
Wessel Oosterbroek <w.oosterbroek@student.tudelft.nl>
2023-03-27 10:36:01 +0200

    Clean-up, insert inserted base(s) into genotype

--- generate_figures.py
+++ generate_figures.py
@@ -4,10 +4,11 @@ import numpy as np
 
 # Generate figure 1e
 def generate_figure_1e(sequence, cutsite):
-
+    pd.set_option('display.max_colwidth', 199) 
 
     # Get predictions and statistics
     pred_df, stats = inDelphi.predict(sequence, cutsite)
+    # print(pred_df)
 
     # Adds the mhless_genotypes
     pred_df = inDelphi.add_mhless_genotypes(pred_df, stats)
@@ -15,12 +16,19 @@ def generate_figure_1e(sequence, cutsite):
     # Adds a genotype colum
     pred_df = inDelphi.add_genotype_column(pred_df, stats)
 
-    # Need because of python version
+    # Needed because of python version
     pred_df = pred_df.rename(columns={'Genotype position': 'Genotype_position'})
 
     # Correctly identify mh-less deletions
-    query = '(Category == \'del\') & (1 <= Length <= 60) & (0 <= Genotype_position <= Length)'
-    pred_df.loc[pred_df.query(query).index,'Category'] = "mh-less del"
+    mhlessQuery = '(Category == \'del\') & (1 <= Length <= 60) & (0 <= Genotype_position <= Length)'
+    pred_df.loc[pred_df.query(mhlessQuery).index,'Category'] = "mh-less del"
+    
+    # In the case of an insertion, add insert base(s) to genotype
+    insertionQuery = '(Category == \'ins\')'
+    pred_df.loc[pred_df.query(insertionQuery).index, 'Genotype'] = pred_df['Genotype'].str[:cutsite] + pred_df['Inserted Bases'] + pred_df['Genotype'].str[cutsite:]
+
+    # deletionQuery = '(Category == \'del\' | Category == \'mh-less del\')'
+    # pred_df.loc[pred_df.query(deletionQuery).index, 'Genotype'] = pred_df['Genotype'].str[:cutsite] + pred_df['Genotype'].str[cutsite:]
 
     # Print data frame
     print(pred_df.sort_values(by='Predicted frequency', ascending=False).head(20)[['Genotype', 'Category', 'Predicted frequency']].to_string(index=False))
