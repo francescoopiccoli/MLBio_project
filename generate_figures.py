266c2c843bb5c37f260eaa6f200db820837a2996
Wessel Oosterbroek <w.oosterbroek@student.tudelft.nl>
2023-03-27 16:21:09 +0200

    Cleanu-up, store result of figure 1e in a png

--- generate_figures.py
+++ generate_figures.py
@@ -1,6 +1,22 @@
 from inDelphiModel import inDelphi
 import pandas as pd
+from pandas.plotting import table 
 import numpy as np
+import matplotlib.pyplot as plt
+
+def render_mpl_table(df, col_width):
+    size = (np.array(df.shape[::-1]) + np.array([0, 1])) * np.array([col_width, 0.7])
+    fig, ax = plt.subplots(figsize=size)
+    ax.axis('off')
+        
+    mpl_table = ax.table(cellText=df.values, bbox=[0, 0, 1, 1], colLabels=df.columns)
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(16)
+
+    mpl_table.auto_set_column_width(col=list(range(len(df.columns))))
+
+    return ax.get_figure()
+
 
 # Generate figure 1e
 def generate_figure_1e(sequence, cutsite):
@@ -27,8 +43,8 @@ def generate_figure_1e(sequence, cutsite):
     insertionQuery = '(Category == \'ins\')'
     pred_df.loc[pred_df.query(insertionQuery).index, 'Genotype'] = pred_df['Genotype'].str[:cutsite] + pred_df['Inserted Bases'] + pred_df['Genotype'].str[cutsite:]
 
-    # deletionQuery = '(Category == \'del\' | Category == \'mh-less del\')'
-    # pred_df.loc[pred_df.query(deletionQuery).index, 'Genotype'] = pred_df['Genotype'].str[:cutsite] + pred_df['Genotype'].str[cutsite:]
+    deletionQuery = '(Category == \'del\' | Category == \'mh-less del\')'
+    pred_df.loc[pred_df.query(deletionQuery).index, 'Genotype'] = pred_df['Genotype'].str[:cutsite] + pred_df['Genotype'].str[cutsite:]
 
     # Print data frame
     print(pred_df.sort_values(by='Predicted frequency', ascending=False).head(20)[['Genotype', 'Category', 'Predicted frequency']].to_string(index=False))
@@ -36,8 +52,12 @@ def generate_figure_1e(sequence, cutsite):
 
     pd.reset_option('display.max_rows')
 
+    showdf = pred_df.sort_values(by='Predicted frequency', ascending=False).head(20)[['Genotype', 'Category', 'Predicted frequency']]
 
+    fig = render_mpl_table(showdf, col_width=12.0)
+    fig.savefig("output/figure_1e.png",dpi=300, bbox_inches = "tight")
 
+# Generate figure 3f
 def generate_figure_3f(predictions):
   pass
   # Get highest ins rate for each sequence
@@ -58,3 +78,6 @@ if __name__ == '__main__':
 
     # Generate figure 1e
     generate_figure_1e(seq, cutsite)
+
+    # Generate figure 3f
+    generate_figure_3f(None)
