6e73c2a73e38943b4469a5a615f0ea731f2e283a
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-03-28 09:56:53 +0200

    feat: add code to plot histograms (image 3f)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy
 import csv
 import inDelphi
 import pandas as pd
-
+import matplotlib.pyplot as plt
 # From Boris: load test targets
 test_targets = {}
 with open('output_test/test_targets.csv') as file:
@@ -12,14 +12,29 @@ with open('output_test/test_targets.csv') as file:
     for row in csvreader:
         test_targets[row[0]] = row[1]
 
-left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
+"""left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
 right_seq = 'CCGTGGACGTGAGAAAGAAGAAACATAATATTCGCACTAGATCCATCCCCATACCTGACC'
 seq = left_seq + right_seq
-cutsite = len(left_seq)
+cutsite = len(left_seq)"""
 
-pred_df, stats = inDelphi.predict(seq, cutsite)
+#           GTTCTACAGATTGCTTG|TAC
+# AACACTCCCTGTTCTACAGATTGCTTG|TACTGGTGAACAACATATTGGTATTCAA
+# iterate over all test targets
+highest_del_freq_list = []
+highest_ins_freq_list = []
+for target in test_targets.values():
+    _, stats = inDelphi.predict(target, 27)
+    highest_del_freq_list.append(stats["Highest del frequency"])
+    highest_ins_freq_list.append(stats["Highest ins frequency"])
 
-pd.options.display.float_format = '{:.2f}'.format
+
+# Make a histogram of the frequencies
+plt.hist(highest_del_freq_list, bins = 100)
+plt.show()
+
+# pred_df, stats = inDelphi.predict(seq, cutsite)
+
+"""pd.options.display.float_format = '{:.2f}'.format
 print(pred_df.tail(4))
 pred_df.sort_values(by = 'Predicted frequency', ascending = False, inplace = True)
-print(pred_df.head(10))
+print(pred_df.head(10))"""
