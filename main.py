3cdc719684964a34b10dd8e25b9551abca6ec513
francescoopiccoli <xfrancescopiccolix@gmail.com>
2023-04-04 17:33:44 +0200

    feat: add method to find observed frequencies

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import sys
 import os
 import numpy
 import csv
-import inDelphi
+from inDelphi import find_observed_freqs
 import pandas as pd
-
 # From Boris: load test targets
 test_targets = {}
 with open('output_test/test_targets.csv') as file:
@@ -16,10 +15,11 @@ left_seq = 'AGAATCGCCCGCGGTCCATCCTTTATCAGCGGGAATTCAAGCGCACCAGCCAGAGGTGTA'
 right_seq = 'CCGTGGACGTGAGAAAGAAGAAACATAATATTCGCACTAGATCCATCCCCATACCTGACC'
 seq = left_seq + right_seq
 cutsite = len(left_seq)
-
+find_observed_freqs(test_targets)
+"""
 pred_df, stats = inDelphi.predict(seq, cutsite)
 
 pd.options.display.float_format = '{:.2f}'.format
 print(pred_df.tail(4))
 pred_df.sort_values(by = 'Predicted frequency', ascending = False, inplace = True)
-print(pred_df.head(10))
+print(pred_df.head(10))"""
