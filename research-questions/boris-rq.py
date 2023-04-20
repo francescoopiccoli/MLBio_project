6467b0a8c11030990961bb85693e2c39879b2086
Boris Berman <bermanboris08@gmail.com>
2023-04-20 17:44:52 +0200

    Add remaining rq code, improve the code in the project

--- research-questions/boris-rq.py
+++ research-questions/boris-rq.py
@@ -13,10 +13,11 @@ import pickle, datetime
 from mylib import util
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
-from Training_model import init_random_params, main_objective, parse_input_data
-from scipy import stats
+from training_model import init_random_params, main_objective, parse_input_data
 import utilities as ut
-import backprop as bp
+from nn_helper import *
+import matplotlib.pyplot as plt
+from scipy import stats
 
 NAME = util.get_fn(__file__)
 
@@ -70,64 +71,6 @@ if __name__ == '__main__':
 
   nn_layer_sizes_new = [2, 32, 32, 16, 1]
   nn2_layer_sizes_new = [1, 16, 16, 1]
-
-
-  def calculate_train_test_scores(seed, swap_train_test, model_unchanged, out_dir):
-    ##
-    # Step 1: Train the model
-    ##
-
-    ans = train_test_split(INP, OBS, OBS2, NAMES, DEL_LENS, test_size = 0.5, random_state = seed)
-    INP_train, INP_test, OBS_train, OBS_test, OBS2_train, OBS2_test, NAMES_train, NAMES_test, DEL_LENS_train, DEL_LENS_test = ans
-
-    if swap_train_test:
-       INP_train, INP_test = INP_test, INP_train
-       OBS_train, OBS_test = OBS_test, OBS_train
-       NAMES_train, NAMES_test = NAMES_test, NAMES_train
-       OBS2_train, OBS2_test = OBS2_test, OBS2_train
-       DEL_LENS_train, DEL_LENS_test = DEL_LENS_test, DEL_LENS_train
-
-    nn_layer_sizes_exp = nn_layer_sizes if model_unchanged else nn_layer_sizes_new
-    nn2_layer_sizes_exp = nn2_layer_sizes if model_unchanged else nn2_layer_sizes_new
-
-    init_nn_params = init_random_params(param_scale, nn_layer_sizes_exp, rs = seed)
-    init_nn2_params = init_random_params(param_scale, nn2_layer_sizes_exp, rs = seed)
-
-    batch_size = 200
-    num_batches = int(np.ceil(len(INP_train) / batch_size))
-    objective = lambda nn_params, nn2_params, iter: main_objective(nn_params, nn2_params, INP_train, OBS_train, OBS2_train, DEL_LENS_train, batch_size, seed)
-    both_objective_grad = grad(objective, argnum=[0,1])
-
-    def print_perf(nn_params, nn2_params, iter):
-        log_out_path = f"{out_dir}/_log_default_model.out" if model_unchanged else f"{out_dir}/_log_modified_model.out"
-        ut.print_and_log(str(iter), log_out_path)
-        if iter % 5 != 0: return None
-        
-        train_loss = main_objective(nn_params, nn2_params, INP_train, OBS_train, OBS2_train, DEL_LENS_train, batch_size, seed)
-        test_loss = main_objective(nn_params, nn2_params, INP_test, OBS_test, OBS2_train, DEL_LENS_test, len(INP_test), seed)
-
-        tr1_rsq, tr2_rsq = ut.rsq(nn_params, nn2_params, INP_train, OBS_train, OBS2_train, DEL_LENS_train, batch_size, seed)
-        te1_rsq, te2_rsq = ut.rsq(nn_params, nn2_params, INP_test, OBS_test, OBS2_test, DEL_LENS_test, len(INP_test), seed)
-        
-        out_line = ' %s  | %.3f\t| %.3f\t| %.3f\t| %.3f\t| %.3f\t| %.3f\t|' % (iter, train_loss, np.mean(tr1_rsq), np.mean(tr2_rsq), test_loss, np.mean(te1_rsq), np.mean(te2_rsq))
-        ut.print_and_log(out_line, log_out_path)
-
-        if iter % 50 == 0:
-            params_out_dir = f"{out_dir}/params-default-model/" if model_unchanged else f"{out_dir}/params-modified-model/"
-            util.ensure_dir_exists(params_out_dir)
-
-            ut.print_and_log(" Iter | Train Loss\t| Train Rsq1\t| Train Rsq2\t| Test Loss\t| Test Rsq1\t| Test Rsq2", log_out_path)
-            ut.print_and_log('%s' % (datetime.datetime.now()), log_out_path)
-            ut.save_parameters(nn_params, nn2_params, params_out_dir, f"iter_{iter}")
-
-    ##
-    # Step 2: Calculate scores (currently just test score)
-    ##
-    
-    nn_params, nn2_params = bp.adam_minmin(both_objective_grad, init_nn_params, init_nn2_params, step_size = step_size, num_iters = num_epochs, callback = print_perf)
-    test_score = main_objective(nn_params, nn2_params, INP_test, OBS_test, OBS2_train, DEL_LENS_test, len(INP_test), seed)
-
-    return test_score
   
   def ten_fold_crossvalidation(model_unchanged = True):
     kf = KFold(n_splits=10, shuffle=True, random_state=42)
@@ -179,14 +122,47 @@ if __name__ == '__main__':
               ut.print_and_log('%s' % (datetime.datetime.now()), log_out_path)
               ut.save_parameters(nn_params, nn2_params, out_fold_dir, f"iter_{iter}")
               ut.print_and_log(f"Fold {i + 1}: {test_loss}", log_dest)
+              scores.append(test_loss)
 
-      nn_params, nn2_params = bp.adam_minmin(both_objective_grad, init_nn_params, init_nn2_params, step_size = step_size, num_iters = num_epochs, callback = print_perf)
-      test_score = main_objective(nn_params, nn2_params, INP_test, OBS_test, OBS2_train, DEL_LENS_test, len(INP_test), seed)
-      scores.append(test_score)
+      nn_params, nn2_params = adam_minmin(both_objective_grad, init_nn_params, init_nn2_params, step_size = step_size, num_iters = num_epochs, callback = print_perf)
+      main_objective(nn_params, nn2_params, INP_test, OBS_test, OBS2_train, DEL_LENS_test, len(INP_test), seed)
+
+    return scores
   
 
   # Perform crossvalidation
   print(f"------ Starting 10-fold cross-validation on both models ------")
   folds_unchanged = ten_fold_crossvalidation()
   folds_changed = ten_fold_crossvalidation(False)
+  print(folds_unchanged)
+  print(folds_changed)
   print(f"------ Cross-validation successfully finished! ------")
+
+  diff = [x1 - folds_changed[i] for i, x1 in enumerate(folds_unchanged)]
+  print(np.median(diff), np.mean(diff), np.std(diff), stats.skew(diff))
+
+  # Output histogram and PDF of the fold differences
+  n, bins, patches = plt.hist(diff, bins=10, density=True, alpha=0.5)
+  x = np.linspace(min(bins), max(bins), 100)
+  pdf = stats.norm.pdf(x, loc=np.mean(diff), scale=np.std(diff))
+
+  plt.plot(x, pdf, color='red', linewidth=2)
+  plt.xlabel('Data')
+  plt.ylabel('Density')
+  plt.show()
+
+  # Output boxplot for the fold differences
+  fig, ax = plt.subplots()
+  ax.boxplot(diff)
+  ax.set_ylabel('Values')
+  plt.show()
+
+  # Do sign test
+  pos = sum(1 for d in diff if d > 0)
+  neg = sum(1 for d in diff if d < 0)
+  p_value = stats.binomtest(min(pos, neg), n=len(diff), alternative='two-sided')
+  print("p-value:", p_value)
+
+  # Do Wilcoxon test
+  res = stats.wilcoxon(diff)
+  print(f"Wilcoxon results (statistic, p-value): {res}")
